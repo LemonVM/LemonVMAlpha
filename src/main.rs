6685f5e6ae94324a18ae5a4fbac95835828efb36
himself65 <himself65@outlook.com>
2020-05-14 15:07:11 +0800

    fixup!

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,6 @@ use std::env;
 use bin_format::*;
 use std::io;
 use rustyline::Editor;
-pub fn get_input(prompt: &str) -> String{
-    println!("{}",prompt.green().bold());
-    let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(_goes_into_input_above) => {},
-        Err(_no_updates_is_fine) => {},
-    }
-    input.trim().to_string()
-}
 
 use async_std::prelude::*;
 #[async_std::main]
